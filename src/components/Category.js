87de0289a62ae54c00547b1dbf16be45f7559230
Johan Swan <jswannieman@gmail.com>
2023-09-30 16:24:08 +1300

    Commit Category Javascript Component

--- src/components/Category.js
+++ src/components/Category.js
@@ -10,12 +10,9 @@ export default function Category() {
       <h2>{category.name} Sessions</h2>
 
       <ul className="session-list">
-        {category?.sessions.map(session => (
+        {category?.sessions.map((session) => (
           <li className="session" key={session.id}>
-            <p className="session-name">{session.name}</p>
-            <p>
-              {session.speaker.name} | {session.speaker.org}
-            </p>
+            <p>{session.desc}</p>
           </li>
         ))}
       </ul>
