7483117fe7b826621782637776304cd993037d52
Johan Swan <jswannieman@gmail.com>
2023-10-01 16:43:06 +1300

    Update Confirmation Javascript Component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -14,6 +14,7 @@ export default function Confirmation() {
           <p>
             We have send a confirmation email to <strong>{state.email}</strong>
           </p>
+          <p>Welcome aboard!</p>
         </div>
       ) : (
         <div>
