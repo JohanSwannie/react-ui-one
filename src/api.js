f6280c6cc5b554e56a0d52bba7f271ef1dd85704
Johan Swan <jswannieman@gmail.com>
2023-10-01 14:20:14 +1300

    Update api Javascript Component

--- src/api.js
+++ src/api.js
@@ -6,12 +6,20 @@ const categories = [
       {
         name: "Wash & Dry Service",
         id: "wash-dry-category1",
+        shortDescr: "Wash & Dry",
         desc: "We take care of the washing and drying of your clothes to a very high standard.",
+        subservice: {
+          extra: "With every 7 orders you get 1 order for free",
+        },
       },
       {
         name: "Fluff, & Fold Service",
         id: "fluff-fold-category02",
+        shortDescr: "Fluff & Fold",
         desc: " In this service your clothes will also be picked directly from your home and delivered back to you after cleaning and drying them with folded nicely.",
+        subservice: {
+          extra: "With every 7 orders you get 1 order for free",
+        },
       },
     ],
   },
@@ -22,16 +30,30 @@ const categories = [
       {
         name: "amet consectetur adipisicing",
         id: "owedkddie01",
+        shortDescr: "Blablabla",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, excepturi provident",
+        subservice: {
+          extra: "With every 7 orders you get 1 order for free",
+        },
       },
       {
         name: "bj3eu3eu3i3",
         id: "oeekf0dkkd02",
+        shortDescr: "Ohohoh",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio et sed enim id ab vitae dolorum consequuntur",
+        subservice: {
+          extra: "With every 7 orders you get 1 order for free",
+        },
       },
     ],
   },
 ];
+
+export function getService({ catId, serviceId }) {
+  return categories
+    .find(({ id }) => id === catId)
+    .services.find(({ id }) => id === serviceId);
+}
 
 export function getCategory(catId) {
   return categories.find(({ id }) => id === catId);
