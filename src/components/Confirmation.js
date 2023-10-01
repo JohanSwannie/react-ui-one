2c5990067193637349424dcf1a60869fa18ec7a7
Johan Swan <jswannieman@gmail.com>
2023-10-01 16:20:02 +1300

    Update Confirmation Javascript Component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function Confirmation() {
+  const { state } = useLocation();
+
   return (
     <div className="container">
       <h1>Thank You!</h1>
       <p>You're now registered for Swannies Laundromat Services</p>
+      <p>
+        We have send a confirmation email to <strong>{state.email}</strong>
+      </p>
     </div>
   );
 }
