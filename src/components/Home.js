46f4f4f1d048a2405158b1de3c5ed924bf9c2b57
Johan Swan <jswannieman@gmail.com>
2024-04-18 14:43:17 +1200

    Update Home Javascript Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,15 @@ export default function Home({ title }) {
       <img
         className="img-feat"
         src={mainImg}
-        alt="Red30 Tech conference attendees on a laptop"
+        alt="Swannies Laundromat Services"
       />
 
       <h1>{title}</h1>
       <p>
-        For that perfectly soft scent of freshness, ee are the right choice for
-        your cloths, where Spotless Clothes Are at Your Doorstep. Let our smooth
-        touch take the stress out of your day, giving you time back for the
-        important things.
+        For that perfectly soft scent of freshness, Swannies Laundromat Services
+        are the right choice for your cloths, where Spotless Clothes Are at Your
+        Doorstep. Let our smooth touch take the stress out of your day, giving
+        you time back for the important things.
       </p>
     </div>
   );
